d599d945529ec4f0aa6efbaa0dfc7340b8d6e4d8
shadowCow <shadowCow84@gmail.com>
2024-08-07 21:00:34 -0400

    moving GameClient to command based calls.

--- Domain/Services/GameClient.cs
+++ Domain/Services/GameClient.cs
@@ -2,6 +2,8 @@ namespace Domain.Services;
 
 public interface IGameClient
 {
+    void Submit(GameClientCommand c);
+
     // Actions
     void CreateGameLobby(string gameId);
     void JoinGameLobby(string lobbyId);
@@ -17,4 +19,17 @@ public interface IGameClient
     bool IsInSession(string sessionId);
     GameServerError? GetLastError();
     GameServerEvent? GetLastEvent();
+}
+
+public abstract record GameClientCommand
+{
+    private GameClientCommand() {}
+
+    public sealed record CreateGameRoom() : GameClientCommand;
+    public sealed record JoinGameRoom(string RoomId) : GameClientCommand;
+    public sealed record CloseGameRoom() : GameClientCommand;
+    public sealed record ListAvailableGames() : GameClientCommand;
+    public sealed record SelectGame(string GameId) : GameClientCommand;
+    public sealed record StartGameSession() : GameClientCommand;
+    public sealed record MakeMove(object Move) : GameClientCommand;
 }
